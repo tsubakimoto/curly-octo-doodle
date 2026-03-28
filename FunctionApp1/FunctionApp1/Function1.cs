b35b1533ad13f9d1d8e2633d2d30bb51f3fccb0c
Yuta Matsumura <tkmbmbr@gmail.com>
2026-03-28 11:17:57 +0900

    Function1: Update Run method to async and add HttpClient usage

--- FunctionApp1/FunctionApp1/Function1.cs
+++ FunctionApp1/FunctionApp1/Function1.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
@@ -15,8 +15,11 @@ public class Function1
     }
 
     [Function("Function1")]
-    public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+    public async Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
     {
+        HttpClient client = new HttpClient();
+        var response = await client.GetAsync("https://example.com");
+
         _logger.LogInformation("C# HTTP trigger function processed a request.");
         return new OkObjectResult("Welcome to Azure Functions!");
     }
